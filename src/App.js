3d410712d1fe12cfb20485db05bf701696cb53ea
Ivan <ivan1501@gmail.com>
2020-03-13 14:19:25 +0100

    Posters of movies are shown properly now.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import MovieRow from './MovieRow.js'
+import MovieRow from './MovieRow.js';
+import $ from 'jquery'
 
 class App extends Component {
 
@@ -27,6 +28,31 @@ class App extends Component {
 }
     perfomSearch() {
       console.log("Perform search using MovieDB API")
+      const urlString = "https://api.themoviedb.org/3/search/movie?query=marvel&api_key=1b5adf76a72a13bad99b8fc0c68cb085"
+      $.ajax({
+        url: urlString,
+        success: (searchResults) => {
+          console.log("Fetched data sucssesfuly")
+          // console.log(searchResults)
+          const results = searchResults.results
+          // console.log(results[0])
+
+          let movieRows = []
+
+          results.forEach((movie) => {
+            movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
+            // console.log(movie.title)
+            const movieRow = <MovieRow movie={movie} />
+            movieRows.push(movieRow)
+          })
+
+          this.setState({rows: movieRows})
+
+        },
+        error: (hxr, status, err) => {
+          console.error("Fialed to fetch data")
+        }
+      })
     }
 
 render() {
