73e6b266d5e8aadb11c8d7a3f57077b089380f02
Ivan <ivan1501@gmail.com>
2020-03-13 14:42:36 +0100

    Now you can search for movies in app. Change in handler function.

--- src/App.js
+++ src/App.js
@@ -24,11 +24,11 @@ class App extends Component {
 
     // this.state = {rows : movieRows}
 
-    this.perfomSearch()
+    this.perfomSearch("Lord of the Rings")
 }
-    perfomSearch() {
+    perfomSearch(searchTerm) {
       console.log("Perform search using MovieDB API")
-      const urlString = "https://api.themoviedb.org/3/search/movie?query=woman&api_key=1b5adf76a72a13bad99b8fc0c68cb085"
+      const urlString = "https://api.themoviedb.org/3/search/movie?&api_key=1b5adf76a72a13bad99b8fc0c68cb085&query=" + searchTerm
       $.ajax({
         url: urlString,
         success: (searchResults) => {
@@ -55,6 +55,13 @@ class App extends Component {
       })
     }
 
+    searchChangeHandler(event) {
+      console.log(event.target.value)
+      const boundObject = this
+      const searchTerm =event.target.value
+      boundObject.perfomSearch(searchTerm)
+    }
+
 render() {
   return (
     <div>
@@ -72,7 +79,14 @@ render() {
         </tbody>
       </table>
    
-      <input className="Search" placeholder="Search for movie" />
+      <input style={{
+        fontsize :25,
+        display: 'block',
+        width: '50%',
+        paddingTop: 5,
+        paddingBottom: 5,
+        paddingLeft: 15
+      }} onChange={this.searchChangeHandler.bind(this)} placeholder= "Search for movie" />
 
     {this.state.rows}
 
