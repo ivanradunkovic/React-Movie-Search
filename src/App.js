5c48552e0064203e06ba9a4e964aeb79cb9a7a63
Ivan <ivan1501@gmail.com>
2020-03-13 15:26:14 +0100

    Coments. Code under coment was used while testing app without API.

--- src/App.js
+++ src/App.js
@@ -8,23 +8,21 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {}
-    // console.log("Default initializer")
 
+    // console.log("Default initializer")
     // const movies = [
     //   {id: 0, poster_src: "https://image.tmdb.org/t/p/w185_and_h278_bestv2/d4KNaTrltq6bpkFS01pYtyXa09m.jpg", title: "The Godfather", overwiev: "Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family."},
     //   {id: 1, poster_src: "https://image.tmdb.org/t/p/w185_and_h278_bestv2/bVq65huQ8vHDd1a4Z37QtuyEvpA.jpg", title: "The Godfather: Part II", overwiev: "In the continuing saga of the Corleone crime family, a young Vito Corleone grows up in Sicily and in 1910s New York"},
     // ]
-
     // let movieRows = []
     // movies.forEach((movie) => {
     //   console.log(movie.title)
     //   const movieRow = <MovieRow movie={movie} />
     // movieRows.push(movieRow)
     // })
-
     // this.state = {rows : movieRows}
 
-    this.perfomSearch("Lord of the Rings") // default search string when you start app
+    this.perfomSearch("1") // default search string when you start app
 }
     perfomSearch(searchTerm) {
       console.log("Perform search using MovieDB API")
