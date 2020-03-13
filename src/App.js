6d6aaac97204dedddf8a980e7db883f373d79a3a
Ivan <ivan1501@gmail.com>
2020-03-13 12:57:41 +0100

    Created array for movies. In rows title of movies will be shown.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props)
+    console.log("Default initializer")
+
+    const movies = [
+      {id: 0, title: "The Godfather", overwiev: "Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family."},
+      {id: 1, title: "The Godfather: Part II", overwiev: "In the continuing saga of the Corleone crime family, a young Vito Corleone grows up in Sicily and in 1910s New York"},
+    ]
+
+    this.state = {rows: [
+      <p key="1">This is row 0</p>,
+      <p key="2">This is row 1</p>,
+      <p key="3">This is row 2</p>
+    ]}
+}
+
+render() {
   return (
     <div className="App">
 
@@ -20,8 +37,11 @@ function App() {
    
       <input className="Search" placeholder="Search for movie" />
 
+    {this.state.rows}
+
     </div>
-  );
+    );
+  }
 }
 
 export default App;
